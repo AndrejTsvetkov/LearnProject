019cf92328d9832c2366c2ac28986c198fef3a0c
Andrey Tsvetkov <cvetkoandrejj@gmail.com>
2021-04-24 19:22:07 +0300

    Solved task in Lecture 5

--- L5_1.py
+++ L5_1.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import itertools
+import functools
 
 def project_stats(path, extensions):
     """
@@ -10,28 +11,35 @@ def project_stats(path, extensions):
     в папке ``path`` (и подпапках), имеющие расширение
     из множества ``extensions``.
     """
-    pass
+    filenames = iter_filenames(path)
+    filenames_with_extensions = list(with_extensions(extensions, filenames))
+    #print(filenames_with_extensions)
+    return functools.reduce(lambda x, y: x + y, list(total_number_of_lines(filenames_with_extensions)))
+
 
 
 def total_number_of_lines(filenames):
     """
     Вернуть общее число строк в файлах ``filenames``.
     """
-    pass
-
+    return map(number_of_lines, filenames)
 
 def number_of_lines(filename):
     """
     Вернуть число строк в файле.
     """
-    pass
+    return sum(1 for line in open(filename, 'r', encoding="utf8"))
 
 
 def iter_filenames(path):
     """
     Итератор по именам файлов в дереве.
     """
-    pass
+    # get filenames of all subdirectories
+    filenames = (os.path.join(root, name) for root, dir, file in os.walk(path) for name in file)
+    # make a flat list out of a list of lists
+    #filenames = list(itertools.chain.from_iterable(filenames_2d))
+    return filenames
 
 
 def with_extensions(extensions, filenames):
@@ -39,12 +47,13 @@ def with_extensions(extensions, filenames):
     Оставить из итератора ``filenames`` только
     имена файлов, у которых расширение - одно из ``extensions``.
     """
-    pass
+    filenames_with_extensions = filter(lambda file: get_extension(file) in extensions, filenames)
+    return filenames_with_extensions
 
 
 def get_extension(filename):
     """ Вернуть расширение файла """
-    pass
+    return os.path.splitext(filename)[-1]
 
 
 def print_usage():
@@ -58,5 +67,10 @@ if __name__ == '__main__':
 
     project_path = sys.argv[1]
     print(project_path)
-    help = "https://stackoverflow.com/questions/3964681/find-all-files-in-a-directory-with-extension-txt-in-python"
-    #print(project_stats(project_path, {'.py'}))
+    print(project_stats(project_path, {'.py'}))
+
+    #test_path = "C:/Users/cvetk/PycharmProjects/pythonProject"
+    #print(project_stats(test_path, {'.py', '.txt'}))
+
+
+
